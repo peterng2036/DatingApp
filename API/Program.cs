f667e1de3acebfb550129f463535105d2fcfc9ce
peterng2036 <kachonng1@gmail.com>
2023-03-02 20:46:36 +0800

    Updated error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,5 @@
-using System.Runtime.Intrinsics.X86;
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +10,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
